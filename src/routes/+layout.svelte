f2c2ff9254e1c76d8af7df2438199b784b9bc6b3
clarinharocha <clararocha.s30@gmail.com>
2025-01-27 22:31:59 -0300

    Alteracoes no codigo.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,18 +4,6 @@
 
 	<nav class="navbar navbar-expand navbar-light">
 		<div class="container-fluid d-flex justify-content-between">
-			<!-- Remover o botão de toggle -->
-			<!-- <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span class="navbar-toggler-icon"></span>
-            </button> -->
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav mb-2 mb-lg-0 mx-auto">
 					<!-- mx-auto garante a centralização -->
